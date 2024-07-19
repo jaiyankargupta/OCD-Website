72b6e3abc1dded47a39d201afb256de3eed1cc05
Naveen Beniwal <naveenbeniwal00001@gmail.com>
2024-07-19 22:48:18 +0530

    updated-ui-popup Improve the UI of Pop-Up #84

--- index.js
+++ index.js
@@ -1,53 +1,48 @@
-const contactForm = document.getElementById('contactForm');
-const formResponse = document.getElementById('formResponse');
-contactForm.addEventListener('submit', function(event) {
-    event.preventDefault();
-    formResponse.style.display = 'block';
-    setTimeout(() => {
-        contactForm.reset();
-        formResponse.style.display = 'none';
-    }, 2000);
+const contactForm = document.getElementById("contactForm");
+const formResponse = document.getElementById("formResponse");
+contactForm.addEventListener("submit", function (event) {
+  event.preventDefault();
+  formResponse.style.display = "block";
+  setTimeout(() => {
+    contactForm.reset();
+    formResponse.style.display = "none";
+  }, 2000);
 });
 
-// JS code for Pop-up section 
+// JS code for Pop-up section
 
-document.addEventListener('DOMContentLoaded', function() {
-    var popupBanner = document.getElementById('popup-banner');
-    var closeBtn = document.getElementById('close-btn');
+document.addEventListener("DOMContentLoaded", function () {
+  var popupBanner = document.getElementById("popup-banner");
+  var closeBtn = document.getElementById("close-btn");
 
-    // Function to show the popup banner
-    function showPopup() {
-        popupBanner.style.display = 'block';
-    }
-
-    // Function to close the popup banner
-    window.addEventListener('click', function() {
-        popupBanner.style.display = 'none';
-    });
-
-    setTimeout(showPopup, 1000);
+  // Function to show the popup banner
+  function showPopup() {
+    popupBanner.style.display = "block";
+  }
+  // function to hide popup banner
+  window.hidepopup = function () {
+    const popup = document.getElementById("popup-banner");
+    popup.style.display = "none";
+  };
+  setTimeout(showPopup, 700);
 });
 
 // JS for dark mode toggle button
 
-document.addEventListener('DOMContentLoaded', () => {
-    
-    const toggleButton = document.getElementById('dark-mode-toggle');
+document.addEventListener("DOMContentLoaded", () => {
+  const toggleButton = document.getElementById("dark-mode-toggle");
 
+  let Count = 0;
 
-    let Count = 0;
+  toggleButton.addEventListener("click", () => {
+    Count++;
 
-    
-    toggleButton.addEventListener('click', () => {
-       
-        Count++;
-
-        if (Count % 2 === 0) {
-            document.body.className = 'light-mode';
-            toggleButton.textContent = 'Dark Mode';
-        } else {
-            document.body.className = 'dark-mode';
-            toggleButton.textContent = 'Light Mode';
-        }
-    });
+    if (Count % 2 === 0) {
+      document.body.className = "light-mode";
+      toggleButton.textContent = "Dark Mode";
+    } else {
+      document.body.className = "dark-mode";
+      toggleButton.textContent = "Light Mode";
+    }
+  });
 });
