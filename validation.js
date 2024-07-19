1d8dbc242e4ed6ed9121660ddf45c0ef6aeada26
sahil-arsss <sahilchaudhary8876@gmail.com>
2024-07-19 22:19:47 +0530

    authentication

--- validation.js
+++ validation.js
@@ -10,14 +10,38 @@ const w_contact=document.getElementById("w-contact");
 
 
 const names=document.getElementById("name");    
-w_name,textContent="";
-if(names.value=="m")
+w_name.textContent="";
+if(names.value=="")
 {
-    w_name.innerText ="name is  required";
+    w_name.innerText="Name required";
+    document.getElementById("w-name").style.color="red";
 }
 
-
 const email=document.getElementById("email");
+
+
+if(email.value=="")
+{
+        w_email.innerText="Email required";
+        document.getElementById("w-email").style.color="red";
+}
 const message=document.getElementById("message");
+if(message.value=="")
+{
+            w_message.innerText="Message required";
+            document.getElementById("w-message").style.color="red";
+}   
 const contact=document.getElementById("contact");
+if(contact.value=="" )
+    {
+                w_contact.innerText="Contact required";
+                document.getElementById("w-contact").style.color="red";
+               
+    }   
+else if ((contact.value).length!=10)
+{
+    w_contact.innerText="Invalid Number";
+    document.getElementById("w-contact").style.color="red";
+       
+}
 }
