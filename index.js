8279d0563d1191e181b79c3dd5fa342109b758dd
priyankagarhwal <garhwalpriyanka69@gmail.com>
2024-07-19 16:41:42 +0530

    fixed navbar

--- index.js
+++ index.js
@@ -9,21 +9,27 @@ contactForm.addEventListener('submit', function(event) {
     }, 2000);
 });
 
-// JS code for Pop-up section 
 
 document.addEventListener('DOMContentLoaded', function() {
     var popupBanner = document.getElementById('popup-banner');
     var closeBtn = document.getElementById('close-btn');
 
-    // Function to show the popup banner
+   
     function showPopup() {
         popupBanner.style.display = 'block';
     }
 
-    // Function to close the popup banner
+    
     closeBtn.addEventListener('click', function() {
         popupBanner.style.display = 'none';
     });
 
     setTimeout(showPopup, 1000);
+    var navbarLinks = document.querySelectorAll('.navbarlink');
+    navbarLinks.forEach(link => {
+        link.addEventListener('click', function() {
+            navbarLinks.forEach(nav => nav.classList.remove('active'));
+            this.classList.add('active');
+        });
+    });
 });
